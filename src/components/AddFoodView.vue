a85cdcb94d389f48ea2daf7caf361acb598aede1
Bamorial <107000074+Bamorial@users.noreply.github.com>
2024-03-22 13:29:14 +0200

    addFood page

--- src/components/AddFoodView.vue
+++ src/components/AddFoodView.vue
@@ -1,15 +1,27 @@
 <script setup>
+import { ref } from 'vue';
+import {supabase} from '../supabaseClient'
 
 
-defineProps({
-  msg: String,
-})
+let foodName= ref('')
+let foodkCal=ref(0)
 
-const count = ref(0)
+async function AddFood(){
+    const res= await supabase.from('food').insert({name:foodName.value, kcal:foodkCal.value})
+    console.log(res)
+}
 </script>
 
 <template>
-  
+    <div class="flex flex-col items-center mt-10 ">
+    <div class="flex flex-col w-[80%] items-center  gap-3">
+
+        <input v-model="foodName" type="text" placeholder="name">
+        <input v-model="foodkCal" type="number" placeholder="kcal">
+        <RouterLink class=" bg-green-500 border-white border-2 text-center foodcard p-2 w-[59%]" to="/food" @click="AddFood()" > Add</RouterLink>
+    </div>
+    </div>
+
 </template>
 
 <style scoped>
--- src/components/FoodCard.vue
+++ src/components/FoodCard.vue
@@ -17,7 +17,7 @@ const props=defineProps({
             <div class=" font-mono text-xl pl-2">{{ props.name }}</div>
             <div class="font-mono text-sm pl-4">{{ props.kcal }} kCal</div>
         </div>
-        <div class="h-4 w-4 bg-white border-black border-2 mr-4 mt-4"></div>
+
     </div>
 </div>  
 </template>
