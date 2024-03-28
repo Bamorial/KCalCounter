c30c65072b737aa14b0cec6099678e8fea543380
Bamorial <107000074+Bamorial@users.noreply.github.com>
2024-03-28 16:30:12 +0200

    search bar

--- src/components/AddNewFood.vue
+++ src/components/AddNewFood.vue
@@ -8,6 +8,7 @@ import { useRoute, useRouter } from 'vue-router';
 
 let route=useRoute()
 let router=useRouter()
+let searchInput
 let pageId=route.params.id
 let foodList=ref([])
 let currentId=-1
@@ -34,11 +35,24 @@ let res= await supabase.from('food_item').insert([{scan_id:pageId, food_id:id}])
 console.log(res)
 router.go(-1)
 }
+async function  Search(){
+  console.log(searchInput)
+let res= await supabase.from('food').select().ilike('name','%'+searchInput+'%')
+foodList.value=res.data
+console.log(res)
+}
 
 
 </script>
 
 <template>
+  <div class="flex justify-center mt-4 gap-4 sticky top-2">
+
+<input type="text" @change="Search()" v-model="searchInput" placeholder="search" class="w-[80%] border-2 bg-slate-200 text-slate-500  border-white foodcard p-3 text-center  font-mono ">
+    
+
+
+</div>
     <div class=" flex flex-col gap-6 items-center justify-center mt-10 mb-[30%]">
 <FoodCard @click="Select(food.id)" v-for="food in foodList" :name="food.name" :kcal="food.kcal"></FoodCard>
 </div>
